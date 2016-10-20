c29253efd16ed495ef5ee7210bcc957677be1150
Luis Llamas <lfllamas93@gmail.com>
2016-10-20 11:32:24 +0200

    Added messaging module

---
+++ messaging/src/main/java/com/mytechia/robobo/framework/hri/messaging/IMessagingModule.java
@@ -0,0 +1,27 @@
+package com.mytechia.robobo.framework.hri.messaging;
+
+import android.graphics.Bitmap;
+
+import com.mytechia.robobo.framework.IModule;
+
+/**
+ * Created by luis on 4/8/16.
+ */
+public interface IMessagingModule extends IModule {
+
+
+    /**
+     * Sends a text via email
+     * @param text The message
+     * @param addresee The destinatary (<someone>@<something>.<domain>)
+     */
+    void  sendMessage(String text,String addresee);
+
+    /**
+     * Sends a text via email with an attached image
+     * @param text The message
+     * @param addresee The destinatary (<someone>@<something>.<domain>)
+     * @param photoAtachment The image to be attached to the message
+     */
+    void  sendMessage(String text,String addresee, Bitmap photoAtachment);
+}
